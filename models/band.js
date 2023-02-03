888fd145658ca287541fa30c19f2eee761347d40
ngard1234 <neilgardner@verizon.net>
2023-02-03 18:43:40 -0500

    complete migration sequelize tabe.

---
+++ models/band.js
@@ -0,0 +1,57 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Band extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  // Band.init({
+  //   band_id: DataTypes.INTEGER,
+  //   name: DataTypes.STRING,
+  //   genre: DataTypes.TEXT,
+  //   available_start_time: DataTypes.DATE,
+  //   end_time: DataTypes.DATE
+  // }, {
+  //   sequelize,
+  //   modelName: 'Band',
+  // });
+
+  Band.init({
+    band_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    genre: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    available_start_time: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    end_time: {
+        type: DataTypes.DATE,
+        allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'Band',
+    tableName: 'bands',
+    timestamps: false
+  })
+
+  return Band;
+};
